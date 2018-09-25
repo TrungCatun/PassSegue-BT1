d48b3339ebdc0fad0e3ad1b9370eb6bb8e787d7f
Trung <>
2018-09-25 07:28:15 +0700

    pass data segue

--- PassSegue-BT1/MasterViewController.swift
+++ PassSegue-BT1/MasterViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MasterViewController: UIViewController {
 
+    @IBOutlet weak var inputTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let x = segue.destination as? DetianViewController
+        x?.data = inputTextField.text
+    }
 
 }
 
